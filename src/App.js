d09448ab02df60d1212d3ad0d9187da977cac41b
Keenan Botes <kee.bot@icloud.com>
2022-12-02 19:53:46 +0200

    Revert "Rockets"

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Mission from './pages/mission';
-import Profile from './pages/profile';
-import Rocket from './components/Rockets/rockets';
-
-import './style/app.css';
+import NavbarVariable from './components/Navbar/Navbar';
+import './App.css';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Rocket />} />
-        <Route path="/mission" element={<Mission />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
+      <NavbarVariable />
     </div>
   );
 }
--- src/pages/profile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const profile = () => (
-  <div>
-    <h1>welcome</h1>
-  </div>
-);
-
-export default profile;
